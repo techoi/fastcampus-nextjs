7595bcef08fe6601d937c7ad20f22d148eb77656
Jimmy Choi <jimmy@tossinvest.com>
2022-06-23 04:25:49 +0900

    feat: Layout 활용

---
+++ nextjs-blog/components/SubLayout.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function SubLayout({ children }) {
+  return (
+    <div>
+      <h1><Link href="/"><a>Home 으로</a></Link></h1>
+      {children}
+    </div>
+  )
+}
