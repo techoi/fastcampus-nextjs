59c9d18a5862c0f41001c8524f92e66e66bd567b
Jimmy Choi <jimmy@tossinvest.com>
2022-06-28 03:34:25 +0900

    feat: Image / Metadata / Layout / Styling

--- blog/pages/posts/first-post.js
+++ blog/pages/posts/first-post.js
@@ -1,3 +1,13 @@
+import Head from 'next/head'
+import Layout from '../../components/Layout'
+
 export default function FirstPost() {
-  return <h1>첫번째 글</h1>
+  return (
+    <Layout>
+      <Head>
+        <title>첫번째 글</title>
+      </Head>
+      <h1>첫번째 글</h1>
+    </Layout>
+  )
 }
