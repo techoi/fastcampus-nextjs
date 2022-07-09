d522da5fa7ef166243708bbcf0bcc61afd7a44bc
Jimmy Choi <jimmy@tossinvest.com>
2022-07-10 05:34:52 +0900

    feat: reportWebVitals

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -4,6 +4,10 @@ import { useState } from 'react'
 import '../styles/global.css'
 import { formatDistanceToNow } from 'date-fns'
 
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
+
 export default function App({ Component, pageProps }) {
   const router = useRouter()
   const [visitedTime] = useState(new Date())
